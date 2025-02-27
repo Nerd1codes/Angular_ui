ee1e0a7d6c9ed4f2257c9f03506e36f8e2f64c07
Nikhil-Zer0 <nikhs114@gmail.com>
2025-02-27 20:36:39 +0530

    got the basic angular ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import {
-  AdaptRxUploaderComponent,
-  FileObj,
-  FileSize,
-  UploaderMode,
-  UploaderSelectionMode,
-  AdaptDeviceDetectionService
-} from '@bmc-ux/adapt-angular';
-import { DemoComponentState } from '../../utils/common-state.enum';
-import { adaptLog } from '../../utils/logger';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'adapt-rx-uploader-demo',
-  templateUrl: './rx-uploader.demo.html'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AdaptRxUploaderDemoComponent {
-  disabled: boolean = false;
-  readonly: boolean = false;
-  isDnd: boolean = true;
-  isMultiple: boolean = true;
-  disabledMode: boolean = false;
-  minFileSize: string = FileSize.MB.toString();
-  uploaderModel: FileObj[] = [];
-
-  state: string[] = Object.values(DemoComponentState);
-  currentRxUploaderState: DemoComponentState = DemoComponentState.DEFAULT;
-
-  mode: typeof UploaderSelectionMode = UploaderSelectionMode;
-  currentUploaderMode: UploaderSelectionMode = UploaderSelectionMode.File;
-
-  type: typeof UploaderMode = UploaderMode;
-  currentUploadType: UploaderMode = UploaderMode.Auto;
-
-  isMobile: boolean;
-
-  @ViewChild(AdaptRxUploaderComponent) uploader: AdaptRxUploaderComponent;
-
-  get uploadLimit(): boolean {
-    return this.uploaderModel && this.uploaderModel.length === this.uploader.filesCount
-      && this.uploaderModel.every(file => file.uploaded === 100);
-  }
-
-  constructor(private _deviceDetectorService: AdaptDeviceDetectionService) {
-    this.isMobile = this._deviceDetectorService.deviceMobile();
-  }
-
-  stateChanged(data: DemoComponentState): void {
-    this.disabled = data === DemoComponentState.DISABLED;
-    this.readonly = data === DemoComponentState.READONLY;
-  }
-
-  modeChanged(): void {
-    this.uploaderModel = [];
-  }
-
-  multipleDisabled(): void {
-    if (!this.isMultiple) {
-      this.disabledMode = true;
-      this.uploaderModel = [];
-      return;
-    }
-    this.disabledMode = false;
-  }
-
-  uploadFiles(): void {
-    this.uploader.startUploading();
-  }
-
-  onFinishedFileUploading(files: FileObj[]): void {
-    adaptLog('FINISHED FILE UPLOADING: ', files);
-  }
-
-  onEndFileUploading(file: FileObj): void {
-    adaptLog('END FILE UPLOADING: ', file);
-  }
+export class AppComponent {
+  title = 'Angular_ui';
 }
--- src/app/module.ts
+++ src/app/module.ts
@@ -1,53 +1,53 @@
-import {HttpClient} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {
-  AdaptCodeViewerModule,
-  AdaptModalModule,
-  AdaptRxCheckboxModule,
-  AdaptRxLabelModule,
-  AdaptRxRadiobuttonModule,
-  AdaptRxUploaderModule,
-  AdaptRxValidatorsModule
-} from '@bmc-ux/adapt-angular';
-import {ComponentChildRoutes} from '../../app.routes';
-import {DocsEnumToArrayPipeModule} from '../../pages/shared/pipes/enum-to-array.pipe';
-import {UCEModule} from '../uce/uce.module';
+// import {HttpClient} from '@angular/common/http';
+// import {NgModule} from '@angular/core';
+// import {CommonModule} from '@angular/common';
+// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+// import {RouterModule} from '@angular/router';
+// import {
+//   AdaptCodeViewerModule,
+//   AdaptModalModule,
+//   AdaptRxCheckboxModule,
+//   AdaptRxLabelModule,
+//   AdaptRxRadiobuttonModule,
+//   AdaptRxUploaderModule,
+//   AdaptRxValidatorsModule
+// } from '@bmc-ux/adapt-angular';
+// import {ComponentChildRoutes} from '../../app.routes';
+// import {DocsEnumToArrayPipeModule} from '../../pages/shared/pipes/enum-to-array.pipe';
+// import {UCEModule} from '../uce/uce.module';
 
-import {AdaptRxUploaderDemoComponent} from './rx-uploader.demo';
-import {RX_UPLOADER_EXAMPLES} from './rx-uploader.example';
-import {UploaderService} from '../../utils/uploader.service';
+// import {AdaptRxUploaderDemoComponent} from './rx-uploader.demo';
+// import {RX_UPLOADER_EXAMPLES} from './rx-uploader.example';
+// import {UploaderService} from '../../utils/uploader.service';
 
-function UploaderFactory(http: HttpClient): UploaderService {
-  return new UploaderService(http);
-}
+// function UploaderFactory(http: HttpClient): UploaderService {
+//   return new UploaderService(http);
+// }
 
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(ComponentChildRoutes),
-    AdaptRxCheckboxModule,
-    AdaptRxLabelModule,
-    AdaptRxRadiobuttonModule,
-    AdaptModalModule,
-    AdaptRxUploaderModule,
-    DocsEnumToArrayPipeModule,
-    AdaptRxValidatorsModule,
-    UCEModule,
-    AdaptCodeViewerModule
-  ],
-  declarations: [...RX_UPLOADER_EXAMPLES, AdaptRxUploaderDemoComponent],
-  providers: [
-    {
-      provide: 'UploaderService',
-      useFactory: UploaderFactory,
-      deps: [HttpClient]
-    }
-  ]
-})
-export class DocsRxUploaderModule {
-}
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     RouterModule.forChild(ComponentChildRoutes),
+//     AdaptRxCheckboxModule,
+//     AdaptRxLabelModule,
+//     AdaptRxRadiobuttonModule,
+//     AdaptModalModule,
+//     AdaptRxUploaderModule,
+//     DocsEnumToArrayPipeModule,
+//     AdaptRxValidatorsModule,
+//     UCEModule,
+//     AdaptCodeViewerModule
+//   ],
+//   declarations: [...RX_UPLOADER_EXAMPLES, AdaptRxUploaderDemoComponent],
+//   providers: [
+//     {
+//       provide: 'UploaderService',
+//       useFactory: UploaderFactory,
+//       deps: [HttpClient]
+//     }
+//   ]
+// })
+// export class DocsRxUploaderModule {
+// }
